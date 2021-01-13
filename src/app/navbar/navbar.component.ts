70836f51a694dd905f85a251bf54f30c2e14c2ce
aman1210 <aman.srivastava101@gmail.com>
2021-01-13 12:26:10 +0530

    initial navbar added

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const number =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+    if (number > 60) {
+      var n = document.querySelector('.navbar');
+      n.classList.add('down');
+      var nb = document.querySelector('.navbar-brand');
+      nb.classList.add('down');
+    } else if (number < 60) {
+      var n = document.querySelector('.navbar');
+      n.classList.remove('down');
+      var nb = document.querySelector('.navbar-brand');
+      nb.classList.remove('down');
+    }
   }
-
 }
