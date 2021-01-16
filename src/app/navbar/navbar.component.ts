250e276c9cf26dc9d7818faa93a8fc48ad38297b
aman1210 <aman.srivastava101@gmail.com>
2021-01-16 09:54:11 +0530

    updating grid and navbar properties

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,25 +7,17 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   constructor() {}
+  prevScrollpos = window.pageYOffset;
 
   ngOnInit(): void {}
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    const number =
-      window.pageYOffset ||
-      document.documentElement.scrollTop ||
-      document.body.scrollTop ||
-      0;
-    if (number > 20) {
-      var n = document.querySelector('.navbar');
-      n.classList.add('down');
-      var nb = document.querySelector('.navbar-brand');
-      nb.classList.add('down');
-    } else if (number < 60) {
-      var n = document.querySelector('.navbar');
-      n.classList.remove('down');
-      var nb = document.querySelector('.navbar-brand');
-      nb.classList.remove('down');
+    const newPos = window.pageYOffset;
+    if (this.prevScrollpos > newPos) {
+      document.querySelector('.navbar').classList.remove('down');
+    } else {
+      document.querySelector('.navbar').classList.add('down');
     }
+    this.prevScrollpos = newPos;
   }
 }
